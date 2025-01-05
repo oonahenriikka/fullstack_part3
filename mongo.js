fc414421ab8d20d2ea94884d025652f40693610b
Oona Nykänen <oona.nykanen@edu.turkuamk.fi>
2025-01-05 20:09:35 +0200

    MongoDB URI:n päivitys

--- mongo.js
+++ mongo.js
@@ -9,8 +9,7 @@ const password = process.argv[2]
 const name = process.argv[3]
 const number = process.argv[4]
 
-const url =
-  `mongodb+srv://oonanykanen:${password}@cluster0.je8xz.mongodb.net/Puhelinluettelo?retryWrites=true&w=majority&appName=Cluster0y`
+const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
